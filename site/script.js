3a45dc46f8dfd65d0eb0197298c10d4635e76948
JULIO SENA <gm.sena@hotmail.com>
2023-02-05 17:10:14 -0300

    EXERCÍCIOS DE JAVASCRIPT

---
+++ site/script.js
@@ -0,0 +1,12 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age; 
+    }
+}
+
+let p1 = new Person("joão",20)
+let p2 = new Person("maria", 30)
+let p3 = new Person("Pedro",40)
+
+console.log(p2.name);
