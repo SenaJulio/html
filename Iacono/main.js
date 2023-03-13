c88c981173fa7468e9767ae2242078917966b6bd
JULIO SENA <gm.sena@hotmail.com>
2023-03-13 17:49:03 -0300

    exercicios javascripr

---
+++ Iacono/main.js
@@ -0,0 +1,26 @@
+
+let grade = prompt('Digite a nota do aluno:')
+
+if (grade > 100) {
+    alert('Nota invalida')
+}else if (grade >=90) {
+    alert(' Aluno Nota A')
+}else if (grade >= 80) {
+    alert(' Aluno Nota B')
+}else if (grade >=70) {
+    alert(' Aluno Nota C')
+}else if (grade >= 60) {
+    alert('Aluno nota D')
+}
+else{
+    alert('Aluno nota F')
+}
+
+
+
+let nota = prompt('Digite a nota do aluno')
+
+if (nota <=100 && nota >=90) {
+
+    
+}
