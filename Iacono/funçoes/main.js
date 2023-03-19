1e027b44768868ad930699d1c846c95a1e2e6b33
JULIO SENA <gm.sena@hotmail.com>
2023-03-19 17:27:05 -0300

    exercicios

---
+++ Iacono/funçoes/main.js
@@ -0,0 +1,17 @@
+
+function changeColor() {
+    let titleH1 = document.getElementById('changeh1')
+    titleH1.style.color = 'red'
+}
+
+
+function greeting(name) {
+    console.log('Hi'  +  name);
+}
+greeting('Julio')
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+  let result = sum(10, 20)
+  console.log(result);
